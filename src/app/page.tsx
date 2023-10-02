06311ee92e95e303ccf9e2d3067b596107bd4f4b
NGustavo011 <gsn181200@gmail.com>
2023-10-02 11:10:33 -0300

    feat: add base random-team-generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <body>
+      <div id="div-root">
+        <div id="title">
+            <h1>Gerador de times</h1>
+        </div>
+        <div id="input-data">
+            <form id="input-form">
+                <div id="team-quantity">
+                    <span>
+                        Quantidade de times:
+                    </span>
+                    <input id="team-quantity-input" type="number" />
+                </div>
+                <div id="players">
+                    <span>
+                        Jogadores (%nome/%nível):
+                    </span>
+                    <textarea id="players-input"></textarea>
+                </div>
+                <div id="submit">
+                    <input id="submit-form" type="submit" value="Submit" />
+                </div>
+            </form>
+        </div>
+        <div id="result">
+        </div>
+      </div>
+    </body>
   )
 }
